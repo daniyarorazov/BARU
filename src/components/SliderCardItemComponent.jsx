7306447c42156245c4be5b1cb7d6010f9824eb5f
Daniyar Orazov <orazov.video@gmail.com>
2022-08-18 20:50:38 +0600

    Updated Slider Plugin

--- src/components/SliderCardItemComponent.jsx
+++ src/components/SliderCardItemComponent.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import "../styles/SliderCardItemComponent.css";
+import ClockIcon from "../images/icons/clock.svg";
 
 function SliderCardItemComponent(props) {
     return (
         <div className="card_block">
             <img className="card__img" src={props.imgSrc} alt="" />
             <p className="card__title">{props.title}</p>
-            <span className="card__date">{props.dateOfEvent}</span>
+            <div>
+                <img className="date__icon" src={ClockIcon} alt="" />
+                <span className="card__date">{props.dateOfEvent}</span>
+            </div>
         </div>
     );
 }
--- src/components/SliderComponent.jsx
+++ src/components/SliderComponent.jsx
@@ -1,116 +1,73 @@
 import React from 'react';
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation, EffectFade  } from "swiper";
 import SliderCardItemComponent from './SliderCardItemComponent';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "swiper/css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/effect-fade';
+
 
 function SliderComponent(props) {
-    const settings = {
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        centerMode: true,
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 2,
-                initialSlide: 2
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-          ]
-      };
+  const datas = [
+    {
+        id: 1,
+        title: "Главные мероприятия недели в Алматы (15 — 21 августа)",
+        imgSrc: "https://images.unsplash.com/photo-1515263487990-61b07816b324?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        dateOfEvent: "16 августа"
+    },
+    {
+        id: 2,
+        title: "Главные мероприятия недели в Алматы (15 — 21 августа)",
+        imgSrc: "https://images.unsplash.com/photo-1531077386034-d12285794b5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        dateOfEvent: "16 августа"
+    },
+    {
+        id: 3,
+        title: "Главные мероприятия недели в Алматы (15 — 21 августа)",
+        imgSrc: "https://images.unsplash.com/photo-1531591022136-eb8b0da1e6d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80",
+        dateOfEvent: "16 августа"
+    },
+    {
+        id: 4,
+        title: "Главные мероприятия недели в Алматы (15 — 21 августа)",
+        imgSrc: "https://images.unsplash.com/photo-1543722530-d2c3201371e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80",
+        dateOfEvent: "16 августа"
+    },
+    {
+        id: 5,
+        title: "Главные мероприятия недели в Алматы (15 — 21 августа)",
+        imgSrc: "https://images.unsplash.com/photo-1543722530-d2c3201371e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80",
+        dateOfEvent: "16 августа"
+    },
+    {
+        id: 6,
+        title: "Главные мероприятия недели в Алматы (15 — 21 августа)",
+        imgSrc: "https://images.unsplash.com/photo-1543722530-d2c3201371e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80",
+        dateOfEvent: "16 августа"
+    },
+]
+const listItems = datas.map((data) => 
+  <SwiperSlide><SliderCardItemComponent title={data.title} imgSrc={data.imgSrc} dateOfEvent={data.dateOfEvent} /></SwiperSlide>
+);
 
-    const datas = [
-        {
-            id: 1,
-            title: "Главные мероприятия недели в Алматы (15 — 21 августа)",
-            imgSrc: "https://images.unsplash.com/photo-1543722530-d2c3201371e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80",
-            dateOfEvent: "16 августа"
-        },
-        {
-            id: 2,
-            title: "Главные мероприятия недели в Алматы (15 — 21 августа)",
-            imgSrc: "https://images.unsplash.com/photo-1543722530-d2c3201371e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80",
-            dateOfEvent: "16 августа"
-        },
-        {
-            id: 3,
-            title: "Главные мероприятия недели в Алматы (15 — 21 августа)",
-            imgSrc: "https://images.unsplash.com/photo-1543722530-d2c3201371e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80",
-            dateOfEvent: "16 августа"
-        },
-        {
-            id: 4,
-            title: "Главные мероприятия недели в Алматы (15 — 21 августа)",
-            imgSrc: "https://images.unsplash.com/photo-1543722530-d2c3201371e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80",
-            dateOfEvent: "16 августа"
-        },
-        {
-            id: 5,
-            title: "Главные мероприятия недели в Алматы (15 — 21 августа)",
-            imgSrc: "https://images.unsplash.com/photo-1543722530-d2c3201371e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80",
-            dateOfEvent: "16 августа"
-        },
-        {
-            id: 6,
-            title: "Главные мероприятия недели в Алматы (15 — 21 августа)",
-            imgSrc: "https://images.unsplash.com/photo-1543722530-d2c3201371e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80",
-            dateOfEvent: "16 августа"
-        },
-    ]
-    return (
-        <div className="container">
-        <Slider {...settings}>
-
-            {/* {datas.map((event, key) => {
-                <SliderCardItemComponent 
-                imgSrc={"https://images.unsplash.com/photo-1435575653489-b0873ec954e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"} 
-                title={"Test"}
-                dateOfEvent={"16 december"}
-            />
-            })} */}
-
-            
-            <SliderCardItemComponent 
-                imgSrc={"https://images.unsplash.com/photo-1515263487990-61b07816b324?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"} 
-                title={"Test"}
-                dateOfEvent={"16 december"}
-            />
-            <SliderCardItemComponent 
-                imgSrc={"https://images.unsplash.com/photo-1531077386034-d12285794b5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"} 
-                title={"Test"}
-                dateOfEvent={"16 december"}
-            />
-            <SliderCardItemComponent 
-                imgSrc={"https://images.unsplash.com/photo-1531591022136-eb8b0da1e6d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80"} 
-                title={"Test"}
-                dateOfEvent={"16 december"}
-            />
-            
-        </Slider>
-      </div>
-    );
+return (
+    <div className="container">
+   <Swiper 
+    className={"mySwiper"}
+    cssMode={true}
+    loop={true}
+    navigation={true}
+    slidesPerView={3}
+    modules={[Pagination, Navigation, EffectFade]}
+    pagination={{dynamicBullets: true, clickable: true}} 
+    
+    >
+        {listItems}
+   </Swiper>
+    </div>
+);
 }
 
 export default SliderComponent;
