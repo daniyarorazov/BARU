41cc37af87f54e5ae281f5772c83112ad9e032a5
DayTroy <shadowyguy5@gmail.com>
2022-08-22 16:05:09 +0300

    Created layout footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import './App.scss';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Footer/>
     </div>
   );
 }
