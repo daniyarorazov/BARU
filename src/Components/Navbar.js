63f2519bf7b130f084e8f4dfc7a95bc7da762539
DayTroy <shadowyguy5@gmail.com>
2022-08-22 14:48:53 +0300

    Fixed all comments and added new features

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,25 +1,25 @@
 import React from "react";
-import calendarIcon from '../images/calendar-icon.svg';
-import profileIcon from '../images/profile-icon.svg';
-import styles from './Navbar.module.css'
+import calendarIcon from '../images/icons/calendar-icon.svg';
+import profileIcon from '../images/icons/profile-icon.svg';
+import styles from './Navbar.module.scss'
 import NavbarSearch from "./NavbarSearch";
 
 function Navbar() {
   return (
-    <div className={styles.navbar}>
-      <a href = "#" className={styles.navbar__header}>
-        <div className={styles.navbar__header__logo}></div>
-        <p className={styles.navbar__header__title}>BARU</p>
+    <nav className={styles.navbar}>
+      <a href = "#" className={styles['navbar-header__link']}>
+        <div className={styles['navbar-header__logo']}></div>
+        <p className={styles['navbar-header__title']}>BARU</p>
       </a>
       <NavbarSearch/>
-      <a href = "#" className={styles.navbar__link}>
-        <img src={calendarIcon} alt="poster" className={styles.navbar__link__icon}></img>
-        <p className={styles.navbar__link__title}>Poster</p>
+      <a href = "#" className={styles['navbar-poster']}>
+        <img src={calendarIcon} alt="poster" className={styles['navbar-poster__icon']}/>
+        <p className={styles['navbar-poster__title']}>Poster</p>
       </a>
-      <a href="#" className={styles.navbar__profile}>
-        <img src={profileIcon} alt="profile" className={styles.navbar__profile__icon}></img>
+      <a href="#" className={styles['navbar-profile']}>
+        <img src={profileIcon} alt="profile" className={styles['navbar-profile__icon']}/>
       </a>
-    </div>
+    </nav>
   );
 }
 
--- src/Components/NavbarSearch.js
+++ src/Components/NavbarSearch.js
@@ -1,11 +1,11 @@
 import React from "react";
-import styles from './NavbarSearch.module.css'
+import styles from './NavbarSearch.module.scss'
 
 function NavbarSearch() {
     return (
-    <div className={styles.navbar__search}>
-        <form className={styles.search__form}>
-            <input className={styles.search__form__input} placeholder="Search..."></input>
+    <div className={styles['navbar-search']}>
+        <form className={styles['search-form']}>
+            <input className={styles['search-form__input']} placeholder="Search..."/>
         </form>
     </div>
     );
